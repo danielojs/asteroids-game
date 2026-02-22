a84d05bb203d7df3fb0cf1c42e400b8eedf6bf4d
danielojs <danielojsandil@gmail.com>
2026-02-22 16:32:21 +0700

    Add Player class and render ship triangle

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import pygame
-from constants import *
+
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     print("Starting Asteroids with pygame version: 2.6.1")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -15,14 +18,17 @@ def main():
 
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
+
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60) / 1000.0
-        print(f"Frame time: {dt:.4f} seconds")
+
+        clock.tick(60)
 
 
 if __name__ == "__main__":

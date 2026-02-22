31974aa9fcc5463fc9c972b86a30884cd69acfbd
danielojs <danielojsandil@gmail.com>
2026-02-22 17:44:50 +0700

    Split asteroids into smaller pieces on shot

--- asteroid.py
+++ asteroid.py
@@ -1,7 +1,10 @@
+import random
+
 import pygame
 
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import ASTEROID_MIN_RADIUS, LINE_WIDTH
+from logger import log_event
 
 
 class Asteroid(CircleShape):
@@ -13,3 +16,23 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+
+        log_event("asteroid_split")
+
+        angle = random.uniform(20, 50)
+        velocity_one = self.velocity.rotate(angle)
+        velocity_two = self.velocity.rotate(-angle)
+
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        asteroid_one = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid_two = Asteroid(self.position.x, self.position.y, new_radius)
+
+        asteroid_one.velocity = velocity_one * 1.2
+        asteroid_two.velocity = velocity_two * 1.2
--- main.py
+++ main.py
@@ -50,7 +50,7 @@ def main():
             for shot in list(shots):
                 if asteroid.collides_with(shot):
                     log_event("asteroid_shot")
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
 
         log_state()
